2f0f8feb064a499f038119b72bf87238ec29a8a8
WeirdoKitten <satriapermata12@gmail.com>
2024-04-15 01:38:01 +0700

    rombak website

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
 
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
-})
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
